8ebc9eec1c1ebdc8c062080dd4a8dde5398c5148
angelkapv81@gmail.com <angelkapv81>
2023-03-12 21:12:47 +0200

    Initial commit

--- github.bat
+++ github.bat
@@ -1,10 +1,40 @@
 @echo off
-set /p commit_msg="Enter summary: "
 
-git config --global user.email "angelkapv81"
-git config --global user.name "angelkapv81@gmail.com"
+:: Переменные
+set folder=%1
+set username=angelkapv81
+set email= angelkapv81@gmail.com
 
-git add .
-git commit -m "%commit_msg%"
-git push origin main
 
+:: Если в указанной папке нет репозитория, создаем его
+IF NOT EXIST "%folder%\.git" (
+    cd "%folder%"
+    
+    :: Инициализация Git репозитория
+    git init
+
+    :: Добавление файлов в Git
+    git add .
+
+    :: Создание первого коммита
+    git commit -m "Initial commit"
+
+    :: Связывание локального репозитория с удаленным
+    git remote add origin https://github.com/%username%/%folder%.git
+
+    :: Публикация ветки main
+    git push -u origin main
+) ELSE (
+    :: Если репозиторий уже существует, запрашиваем сообщение для коммита
+    set /p commit_msg="Enter summary: "
+
+    :: Переходим в указанную папку
+    cd "%folder%"
+
+    git config --global user.email "%email%"
+    git config --global user.name "%username%"
+
+    git add .
+    git commit -m "%commit_msg%"
+    git push origin main
+)
