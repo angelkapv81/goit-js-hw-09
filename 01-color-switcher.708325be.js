77d416abcc41c6c5174d9fe668bc40e944a03ba2
angelkapv81 <114183793+angelkapv81@users.noreply.github.com>
2023-03-12 20:19:59 +0000

    Deploying to gh-pages from @ angelkapv81/goit-js-hw-09@006edc65092a9bfeaab4cdf6a320068c1c092bec 🚀

--- 01-color-switcher.708325be.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a;e.addEventListener("click",(()=>{e.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.708325be.js.map
